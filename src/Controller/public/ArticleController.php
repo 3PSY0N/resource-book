35ab8f60be9d1557fa7e80d435a026faf3baeadf
3PSY0N <78256817+3PSY0N@users.noreply.github.com>
2023-07-12 16:57:53 +0200

    refactor: to public and rename from HomeController to ArticleController revert: cache system

---
+++ src/Controller/public/ArticleController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller\public;
+
+use App\Repository\ArticleRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    public const MAX_PER_PAGE = 10;
+
+    #[Route('/', name: 'app_article_index')]
+    public function index(
+        Request $request,
+        ArticleRepository $articleRepository,
+        PaginatorInterface $paginator
+    ): Response
+    {
+        $articles = $articleRepository->findBy([], ['createdAt' => 'DESC']);
+
+        $paginatedArticles = $paginator->paginate(
+            $articles,
+            $request->query->getInt('page', 1),
+            self::MAX_PER_PAGE
+        );
+
+        return $this->render('public/article/index.html.twig', [
+            'articles' => $paginatedArticles
+        ]);
+    }
+
+    #[Route('/search', name: 'app_article_search')]
+    public function search(Request $request, ArticleRepository $articleRepository, PaginatorInterface $paginator): Response
+    {
+        $filter = $request->get('q');
+
+        if (empty($filter)) {
+            return $this->redirectToRoute('app_article_index');
+        }
+
+        $articlesSearch = $articleRepository->search($filter);
+
+        $pagination = $paginator->paginate(
+            $articlesSearch,
+            $request->query->getInt('page', 1),
+            self::MAX_PER_PAGE
+        );
+
+        return $this->render('public/article/index.html.twig', [
+            'articles' => $pagination
+        ]);
+    }
+}
