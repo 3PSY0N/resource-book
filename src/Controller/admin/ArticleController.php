993cb95bbaf7881b24171feff22f080cac566055
3PSY0N <78256817+3PSY0N@users.noreply.github.com>
2023-08-17 07:24:42 +0200

    Move: article to admin directory

---
+++ src/Controller/admin/ArticleController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Controller\admin;
+
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use App\Repository\TagRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Ramsey\Uuid\Uuid;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin/article')]
+class ArticleController extends AbstractController
+{
+    public const MAX_PER_PAGE = 10;
+
+    #[Route('/', name: 'admin_article_index', methods: ['GET'])]
+    public function index(
+        Request            $request,
+        ArticleRepository  $articleRepository,
+        PaginatorInterface $paginator
+    ): Response
+    {
+        $paginatedArticles = $paginator->paginate(
+            $articleRepository->getArticlesWithTags('DESC'),
+            $request->query->getInt('page', 1),
+            self::MAX_PER_PAGE
+        );
+
+        return $this->render('admin/article/index.html.twig', [
+            'articles' => $paginatedArticles
+        ]);
+    }
+
+    #[Route('/new', name: 'admin_article_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, ArticleRepository $articleRepository): Response
+    {
+        $article = new Article();
+
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $article->setUid(Uuid::uuid7()->toString());
+            $articleRepository->save($article, true);
+
+            return $this->redirectToRoute('admin_article_index', ['slug' => $article->getSlug()], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('admin/article/new.html.twig', [
+            'article' => $article,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{slug}', name: 'admin_article_show', methods: ['GET'])]
+    public function show($slug, ArticleRepository $articleRepository): Response
+    {
+        return $this->render('admin/article/show.html.twig', [
+            'article' => $articleRepository->findOneBySlug($slug)
+        ]);
+    }
+
+    #[Route('/{uid}/edit', name: 'admin_article_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Article $article, ArticleRepository $articleRepository, TagRepository $categoryRepository): Response
+    {
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $articleRepository->save($article, true);
+
+            return $this->redirectToRoute('admin_article_edit', ['uid' => $article->getUid()], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('admin/article/edit.html.twig', [
+            'article' => $article,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{uid}', name: 'admin_article_delete', methods: ['POST'])]
+    public function delete(Request $request, Article $article, ArticleRepository $articleRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $article->getUid(), $request->request->get('_token'))) {
+            $articleRepository->remove($article, true);
+        }
+
+        return $this->redirectToRoute('admin_article_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
