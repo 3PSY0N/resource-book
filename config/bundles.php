cb61380af7f377bea0db0b8f057385291bac0600
3PSY0N <78256817+3PSY0N@users.noreply.github.com>
2023-09-06 21:02:30 +0200

    Add: ignition debug tool

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Spatie\SymfonyIgnitionBundle\IgnitionBundle::class => ['dev' => true],
 ];
